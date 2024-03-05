fc71055408bc74018a7be5f74285f3bc5d7bb826
jamejob <161526293+jamejob@users.noreply.github.com>
2024-03-05 01:23:28 -0500

    Add files via upload

---
+++ Twitter/Twitter/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Twitter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string message = textBox1.Text.Trim();
+
+            if (message.Length > 140)
+            {
+                MessageBox.Show("Error: Message exceeds 140 characters.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Message accepted: " + message, "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
